1ee2396de97b0ff46e83b9bc51395f4a830a82ca
m-a-p-m <matias.pedraza.m@gmail.com>
2025-11-12 14:20:45 -0400

    the whole project lol

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp4Login1P2C
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4Login1P2C
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAceptar_Click(object sender, RoutedEventArgs e)
+        {
+            txtBResultado.Text = lblNombre.Content.ToString() + txtNombre.Text + "\n"
+                + lblEmail.Content.ToString() + txtEmail.Text + "\n"
+                + lblPassword.Content.ToString() + pwdBContra.Password;
+        }
+
+        private void btnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+            txtNombre.Text = "";
+            txtEmail.Text = string.Empty;
+            pwdBContra.Password = string.Empty;
+            txtBResultado.Text = string.Empty;
+        }
+    }
+}
